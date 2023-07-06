9f9885ef614317c83eb4424f6c87c646a04a0c30
BogdanGlazkov <bglazkov@i.ua>
2023-07-06 23:26:44 +0300

    Add Quiz component styles

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -13,23 +13,27 @@ class Quiz extends Component {
           <title>Quiz App - Quiz</title>
         </Helmet>
         <div className="questions">
+          <h1>Quiz Mode</h1>
           <div className="lifeline-container">
             <p>
               <span className="mdi mdi-set-center mdi-24px lifeline-icon"></span>
-              2
+              <span className="lifeline">2</span>
             </p>
             <p>
               <span className="mdi mdi-lightbulb-on-outline mdi-24px lifeline-icon"></span>
-              5
+              <span className="lifeline">5</span>
             </p>
           </div>
-          <div>
+          <div className="lifeline-container">
             <p>
-              <span>1 of 15</span>
-              2:15<span className="mdi mdi-clock-outline mdi-24px"></span>
+              <span className="lifeline">1 of 15</span>
+            </p>
+            <p>
+              <span className="lifeline">2:15</span>
+              <span className="mdi mdi-clock-outline mdi-24px"></span>
             </p>
           </div>
-          <h5>Google was founded in what year?</h5>
+          <h2>Google was founded in what year?</h2>
           <div className="options-container">
             <p className="option">1997</p>
             <p className="option">1998</p>
@@ -39,7 +43,7 @@ class Quiz extends Component {
             <p className="option">2000</p>
           </div>
           <div className="btn-container">
-            <button type="button">Privious</button>
+            <button type="button">Previous</button>
             <button type="button">Next</button>
             <button type="button">Quit</button>
           </div>
