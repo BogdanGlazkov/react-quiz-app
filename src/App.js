a761bbb421f46db4680d5c0d1dc9ecc840f7c452
BogdanGlazkov <bglazkov@i.ua>
2023-07-06 20:51:10 +0300

    Add QuizInstructions component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import QuizInstructions from "./components/QuizInstructions";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/play/instructions" element={<QuizInstructions />} />
       </Routes>
     </BrowserRouter>
   );
